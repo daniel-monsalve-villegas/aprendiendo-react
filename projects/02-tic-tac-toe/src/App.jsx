742ae9cce9779e42254b48a9a0065120967f2b67
daniel-monsalve-villegas <danielmonsalve214164@correo.itm.edu.co>
2023-05-09 10:03:36 -0400

    tic tac toe complete

--- projects/02-tic-tac-toe/src/App.jsx
+++ projects/02-tic-tac-toe/src/App.jsx
@@ -5,10 +5,19 @@ import { TURNS } from './constants';
 import confetti from 'canvas-confetti';
 import { checkWinnerFrom, checkEndGame } from './logic/board';
 import { WinnerModal } from './components/WinnerModal';
+import { saveGameStorage, resetGameStorage } from './logic/storage';
 
 function App() {
-  const [board, setBoard] = useState(Array(9).fill(null));
-  const [turn, setTurn] = useState(TURNS.X);
+  const [board, setBoard] = useState(() => {
+    const boardFromStorage = window.localStorage.getItem('board');
+    return boardFromStorage
+      ? JSON.parse(boardFromStorage)
+      : Array(9).fill(null);
+  });
+  const [turn, setTurn] = useState(() => {
+    const turnFromStorage = window.localStorage.getItem('turn');
+    return turnFromStorage ? JSON.parse(turnFromStorage) : TURNS.X;
+  });
   const [winner, setWinner] = useState(null);
 
   const updateBoard = (index) => {
@@ -19,6 +28,11 @@ function App() {
 
     const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X;
     setTurn(newTurn);
+
+    saveGameStorage({
+      board: newBoard,
+      turn: newTurn,
+    });
 
     const newWinner = checkWinnerFrom(newBoard);
     if (newWinner) {
@@ -33,6 +47,8 @@ function App() {
     setBoard(Array(9).fill(null));
     setTurn(TURNS.X);
     setWinner(null);
+
+    resetGameStorage();
   };
 
   return (
