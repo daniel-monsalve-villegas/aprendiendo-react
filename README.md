916683e9b01c3613df7d535e5867eb779d1f95e1
daniel-monsalve-villegas <danielmonsalve214164@correo.itm.edu.co>
2023-05-10 12:20:36 -0400

    complete

--- README.md
+++ README.md
@@ -2,4 +2,7 @@
 
 Curso de midudev sobre React implementando los conocimientos adquiridos en diferentes proyectos
 
+- [x] Introducción a React
+- [x] React Hooks: useState y useEffect
+- [ ] Prueba técnica
 ### Daniel Monsalve
---
+++ projects/04-prueba-tecnica/App.jsx
@@ -0,0 +1,45 @@
+import { useEffect, useState } from 'react';
+import './App.css';
+
+const CAT_ENDPOINT_RANDOM_FACT = 'https://catfact.ninja/fact';
+/* const CAT_ENDPOINT_IMAGE_URL = `https://cataas.com/cat/says/${firstWord}?size=50&color=red&json=true`; */
+const CAT_PREFIX_IMAGE_URL = 'https://cataas.com';
+
+const App = () => {
+  const [fact, setFact] = useState();
+  const [imageUrl, setImageUrl] = useState();
+
+  useEffect(() => {
+    fetch(CAT_ENDPOINT_RANDOM_FACT)
+      .then((res) => res.json())
+      .then((data) => {
+        const { fact } = data;
+        setFact(fact);
+
+        const firstThreeWord = fact.split(' ', 3).join(' ');
+        fetch(
+          `https://cataas.com/cat/says/${firstThreeWord}?size=50&color=red&json=true`
+        )
+          .then((res) => res.json())
+          .then((data) => {
+            const { url } = data;
+            setImageUrl(url);
+          });
+      });
+  }, []);
+
+  return (
+    <div>
+      <h1>App de gatitos</h1>
+      {fact && <p>{fact}</p>}
+      {imageUrl && (
+        <img
+          src={`${CAT_PREFIX_IMAGE_URL}${imageUrl}`}
+          alt="Image extracted using the first word for fact"
+        />
+      )}
+    </div>
+  );
+};
+
+export default App;
