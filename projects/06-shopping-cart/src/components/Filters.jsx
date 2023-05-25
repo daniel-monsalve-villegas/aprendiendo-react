b94a8463e3b30f9b24738b8017e5edee1ef65d9c
daniel-monsalve-villegas <danielmonsalve214164@correo.itm.edu.co>
2023-05-25 10:34:11 -0400

    filters working but need improve

--- projects/06-shopping-cart/src/components/Filters.jsx
+++ projects/06-shopping-cart/src/components/Filters.jsx
@@ -1,16 +1,41 @@
+import { useState } from 'react'
 import './Filters.css'
 
-export const Filters = () => {
+export const Filters = ({ onChange }) => {
+  const [minPrice, setMinPrice] = useState(0)
+
+  const handleChangeMinPrice = (e) => {
+    setMinPrice(e.target.value)
+    onChange((prevState) => ({
+      ...prevState,
+      minPrice: e.target.value,
+    }))
+  }
+
+  const handleChangeCategory = (e) => {
+    onChange((prevState) => ({
+      ...prevState,
+      category: e.target.value,
+    }))
+  }
+
   return (
     <section className='filters'>
       <div>
         <label htmlFor='price'>Price</label>
-        <input type='range' id='price' min='0' max='1000' />
+        <input
+          type='range'
+          id='price'
+          min='0'
+          max='1000'
+          onChange={handleChangeMinPrice}
+        />
+        <span>{minPrice}</span>
       </div>
 
       <div>
         <label htmlFor='category'>Category</label>
-        <select id='category'>
+        <select id='category' onChange={handleChangeCategory}>
           <option value='all'>All</option>
           <option value='laptops'>Laptops</option>
           <option value='smartphones'>Smartphones</option>
